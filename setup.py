3ef694e843cc760c175fb5ffa70e1c521cf1bb4b
Adib Hasan <nzxcvb5@gmail.com>
2025-10-22 22:01:44 -0400

    setup and package compatibility

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
         "datasets>=2.16.0,<4.0.0",
         "torch-geometric>=2.3.0",
         "torch-sparse",
-        "torch-scatter",
         "pyyaml>=6.0",
         "numpy>=1.24.0,<2.0.0",
         "scikit-learn>=1.2.0",
